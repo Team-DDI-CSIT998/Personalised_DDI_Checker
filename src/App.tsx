707a6f79af36a2377d49a8cb459abf1a6d866745
Fahad Haque <Fahadhaque14@gmail.com>
2025-05-01 21:56:59 +1000

    chatbot designed as a page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import './App.css';
 import MedMatchDoctorPortal from "./components/MedMatchDoctorPortal";
 import PatientDetails from "./components/PatientDetails";
 import CreatePrescription from "./components/CreatePrescription";
+import ChatPagePatient from "./components/chatPagePatient";
 
 function App() {
   return (
@@ -19,10 +20,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><HomePage /></Layout>} />
         <Route path="/Authentication" element={<Layout><Authentication /></Layout>} />
-        
+
         //keep one of the below two routes
         <Route path="/DoctorDashboard" element={<Layout><DoctorDashboard /></Layout>} />
-        <Route path="/MedMatchDoctorPortal" element={<Layout><MedMatchDoctorPortal/></Layout>} />
+        <Route path="/MedMatchDoctorPortal" element={<Layout><MedMatchDoctorPortal /></Layout>} />
 
         <Route path="/DoctorPatientPrescription" element={<Layout><DoctorPatientPrescription /></Layout>} />
         <Route path="/PatientPortal" element={<Layout><PatientPortal /></Layout>} />
@@ -30,6 +31,7 @@ function App() {
         <Route path="/how-it-works" element={<Layout><HowItWorks /></Layout>} />
         <Route path="/patient-details/:patientId" element={<Layout><PatientDetails /></Layout>} />
         <Route path="/create-prescription" element={<Layout><CreatePrescription /></Layout>} />
+        <Route path="/chat-patient" element={<ChatPagePatient />} />
       </Routes>
       <ToastContainer />
     </Router>
