10320e27ae570a6625458a96b5169b0d00e6d139
siddhanth-thakuri <thakurisiddhanth1@gmail.com>
2025-05-24 21:29:31 +1000

    base.ts fixes

--- src/base.ts
+++ src/base.ts
@@ -1,3 +1,3 @@
-export const BASE_URL_1 = import.meta.env.VITE_API_URL || "http://localhost:5000";
-export const BASE_URL_2 = import.meta.env.VITE_API_URL || "http://localhost:8000";
-export const BASE_URL_3 = import.meta.env.VITE_API_URL || "http://localhost:9000";
+export const BASE_URL_1 = import.meta.env.VITE_API_URL_NODE || "http://localhost:5000";
+export const BASE_URL_2 = import.meta.env.VITE_API_URL_FASTAPI1 || "http://localhost:8000";
+export const BASE_URL_3 = import.meta.env.VITE_API_URL_FASTAPI2 || "http://localhost:9000";
